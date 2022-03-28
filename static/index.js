cb06f6ef0037a34a63d3bf1853fba84ccc4ee78c
kimieskitch <101204809+kimieskitch@users.noreply.github.com>
2022-03-27 17:18:33 -0700

    g2g ready 2 roll

---
+++ static/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+
+    // MENU SCROLL EFFECT
+
+    document.querySelectorAll('a[href^="#my_story"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    document.querySelectorAll('a[href^="#in_the_oven"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    document.querySelectorAll('a[href^="#order_details"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    document.querySelectorAll('a[href^="#orders"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    document.querySelectorAll('a[href^="#top"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    
+
+});
